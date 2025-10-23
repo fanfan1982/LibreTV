d6f0e5e2329691829c39ac7df2b7e1f5218e5613
fanfan1982 <98693670+fanfan1982@users.noreply.github.com>
2025-10-23 23:57:05 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,4 +1,5 @@
-const API_SITES = {
+{
+    const API_SITES = {
     dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
@@ -91,12 +92,5 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-       }
+  }
 };
-
-// 调用全局方法合并
-if (window.extendAPISites) {
-    window.extendAPISites(CUSTOMER_SITES);
-} else {
-    console.error;
-}
